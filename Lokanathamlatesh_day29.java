963cb55d4e892dee3b139dd0c295109bcd9a0407
Lokanatham-Latesh <lokanathamlatesh2003@gmail.com>
2023-03-31 20:26:46 +0530

    29coding statement

---
+++ Lokanathamlatesh_day29.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lokanathamlatesh_day29 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Strings ");
+     String str = sc.nextLine();
+     System.out.println("Enter Another the Strings ");
+     String str1 = sc.nextLine();
+    str+=str1;
+     System.out.println("After concatenate String looks like "+str);
+     sc.close();
+}
+    
+}
